200a7ac55baea222aaeb2cdc204bb45972c7ab85
Abel3005 <kimdaegun1114@gmail.com>
2025-04-25 02:50:57 +0900

    api test

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from api.sync_route import router as sync_router
 from api.store_link import router as store_router
 app = FastAPI()
 
-app.include_router(sync_router)
+# app.include_router(sync_router)
 app.include_router(store_router)
